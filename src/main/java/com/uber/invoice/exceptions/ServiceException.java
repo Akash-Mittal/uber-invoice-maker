ec03aa3fbe2ecee107c59289b9b8bd08a16d432a
Akash-Mittal <mittal.akash.adam@gmail.com>
2018-09-23 23:48:48 +0530

    Changes:1.) Exceptions

---
+++ src/main/java/com/uber/invoice/exceptions/ServiceException.java
@@ -0,0 +1,20 @@
+package com.uber.invoice.exceptions;
+
+public class ServiceException extends BaseException {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 8576582334455704149L;
+
+    public ServiceException(String message, Throwable cause) {
+        super(message, cause);
+        // TODO Auto-generated constructor stub
+    }
+
+    public ServiceException(String message) {
+        super(message);
+        // TODO Auto-generated constructor stub
+    }
+
+}
