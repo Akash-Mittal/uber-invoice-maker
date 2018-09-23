a1a8bb97cb349dea1962cb35161b54b5f46c9fa4
Akash-Mittal <mittal.akash.adam@gmail.com>
2018-09-23 23:47:47 +0530

    Changes:1.) Exceptions

---
+++ src/main/java/com/uber/invoice/exceptions/PrinterException.java
@@ -0,0 +1,20 @@
+package com.uber.invoice.exceptions;
+
+public class PrinterException extends BaseException {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -4565969351832232291L;
+
+    public PrinterException(String message, Throwable cause) {
+        super(message, cause);
+        // TODO Auto-generated constructor stub
+    }
+
+    public PrinterException(String message) {
+        super(message);
+        // TODO Auto-generated constructor stub
+    }
+
+}
