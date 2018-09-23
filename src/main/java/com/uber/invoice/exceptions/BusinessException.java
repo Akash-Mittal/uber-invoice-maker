5eafc05350c226fe84e1a7e182d560c2c97b729f
Akash-Mittal <mittal.akash.adam@gmail.com>
2018-09-23 23:47:16 +0530

    Changes:1.) Exceptions

---
+++ src/main/java/com/uber/invoice/exceptions/BusinessException.java
@@ -0,0 +1,15 @@
+package com.uber.invoice.exceptions;
+
+public class BusinessException extends BaseException {
+
+    public BusinessException(String message, Throwable cause) {
+        super(message, cause);
+        // TODO Auto-generated constructor stub
+    }
+
+    public BusinessException(String message) {
+        super(message);
+        // TODO Auto-generated constructor stub
+    }
+
+}
